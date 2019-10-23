b867d8a65f9b8978ad0df022fc00c281d146ff54
Leonardson C. de Carvalho <leonardson.carvalho@gmail.com>
2019-10-22 21:12:42 -0300

    Better organization

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+
+  
+  return (
+    <div className="App">
+      <h1>aa</h1>
+    </div>
+  );
+}
+
+export default App;
